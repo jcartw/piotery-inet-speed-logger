c2ee9d08e221c500f18f26dce22447f438b91e3f
Justin Cartwright <justin.cartwright@technicity.io>
2020-01-16 10:02:05 -0300

    confirm downtime logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
-from lib.inet_speed import run_mock_speed_test
-from lib.weather import get_mock_weather
+from lib.inet_speed import run_speed_test
+from lib.weather import get_weather
 from lib.time_util import get_unix_timestamp
 
 # Classes
@@ -13,8 +13,8 @@ from lib.iotery_conn import IoteryConnection
 import lib.globals as _GLOBALS_
 
 
-REPORT_RATE = 5  # seconds
-INET_CONNECTION_CHECK_RATE = 1000  # seconds
+REPORT_RATE = 30  # seconds
+INET_CONNECTION_CHECK_RATE = 30  # seconds
 
 _GLOBALS_.logger = Logger(stdout_on=False)
 monitor = StatusMonitor()
@@ -34,35 +34,41 @@ while True:
 
     # Check for internet connection
     if time_now > time_next_inet_check:
-        _GLOBALS_.logger.log("INFO: internet connection check begin")
-        inet_is_connected = monitor.inet_is_connected()
-        if inet_is_connected:
-            # reset last connection timestamp
-            time_last_inet_connection = time_now
-            _GLOBALS_.logger.log("Connected to the internet")
-        else:
-            # calculate downtime
-            inet_downtime = time_now - time_last_inet_connection
-            _GLOBALS_.logger.log(
-                f"Not connected to the internet. The current downtime is {inet_downtime} seconds.")
+        try:
+            _GLOBALS_.logger.log("INFO: internet connection check begin")
+            inet_is_connected = monitor.inet_is_connected()
+            if inet_is_connected:
+                # reset last connection timestamp
+                time_last_inet_connection = time_now
+                _GLOBALS_.logger.log("Connected to the internet")
+            else:
+                # calculate downtime
+                inet_downtime = time_now - time_last_inet_connection
+                _GLOBALS_.logger.log(
+                    f"Not connected to the internet. The current downtime is {inet_downtime} seconds.")
 
-        time_next_inet_check = time_now + INET_CONNECTION_CHECK_RATE
+            time_next_inet_check = time_now + INET_CONNECTION_CHECK_RATE
+        except:
+            pass
 
     if time_now > time_next_report and inet_is_connected:
-        _GLOBALS_.logger.log("INFO: iotery data report begin")
-        results_speed_test = run_mock_speed_test()
-        results_weather = get_mock_weather()
+        try:
+            _GLOBALS_.logger.log("INFO: iotery data report begin")
+            results_speed_test = run_speed_test()
+            results_weather = get_weather()
 
-        # report data to iotery
-        iotery_data = {"INTERNET_DOWNTIME": inet_downtime,
-                       "SPEED_TEST_RESULTS": results_speed_test,
-                       "WEATHER_DATA": results_weather}
-        iotery_status = iotery_conn.post_data(data=iotery_data)
+            # report data to iotery
+            iotery_data = {"INTERNET_DOWNTIME": inet_downtime,
+                           "SPEED_TEST_RESULTS": results_speed_test,
+                           "WEATHER_DATA": results_weather}
+            iotery_status = iotery_conn.post_data(data=iotery_data)
 
-        # set timestamp threshold for next report event and reset inet_downtime
-        if iotery_status["status"] == "success":
-            time_next_report = time_now + REPORT_RATE
-            inet_downtime = 0
-            _GLOBALS_.logger.log("SUCCESS: Data report successful.")
+            # set timestamp threshold for next report event and reset inet_downtime
+            if iotery_status["status"] == "success":
+                time_next_report = time_now + REPORT_RATE
+                inet_downtime = 0
+                _GLOBALS_.logger.log("SUCCESS: Data report successful.")
+        except:
+            pass
 
     time.sleep(1.0)
